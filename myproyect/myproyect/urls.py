37db9808a498b2a271a2e0c024cad6f04a89abe6
angelaramiz <angelaramiz22@gmail.com>
2024-06-06 00:39:22 -0600

    paso3

--- myproyect/myproyect/urls.py
+++ myproyect/myproyect/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..mysite.views import add_product, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/', add_product, name='add_product')
 ]
