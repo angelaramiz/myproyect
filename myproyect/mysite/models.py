951e01b36d3dd3222ffe3f54040408e1ff78ca9f
angelaramiz <angelaramiz22@gmail.com>
2024-06-06 00:17:50 -0600

    paso1

--- myproyect/mysite/models.py
+++ myproyect/mysite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price= models.DecimalField(max_digits=10,decimal_places=2)
+    image_url = models.URLField()
+    description = models.TextField()
+    comments = models.FileField()
+    related_products = models.ManyToManyField('self',blank=True)
+
+    def __str__(self):
+        return self.name
