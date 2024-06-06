5da1f3e357c8d12424ea8d121a756098b7693300
angelaramiz <angelaramiz22@gmail.com>
2024-06-06 00:30:00 -0600

    paso2

--- myproyect/mysite/views.py
+++ myproyect/mysite/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ProductForm()
+        return render(request, 'add_product.html', {'form': form})
+    
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
